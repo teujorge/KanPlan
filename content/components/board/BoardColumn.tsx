8980233ea99bc72930571ab090127226ac9cb6fc
Matheus Jorge <mrljorge@outlook.com>
2023-03-05 17:55:24 -0300

    use emotion css

---
+++ content/components/board/BoardColumn.tsx
@@ -0,0 +1,123 @@
+/** @jsxImportSource @emotion/react */
+import BoardCard, { CardInfo } from "./BoardCard";
+import { BoardContext } from "../../../pages/board";
+import { css } from "@emotion/react";
+import { DragEvent, useContext } from "react";
+import {
+  findCardsInColumns,
+  columnsAfterMove,
+} from "../../utilities/boardHelpers";
+
+export type Column = {
+  title: string;
+  cards: CardInfo[];
+};
+
+const BoardColumn = ({ index }: { index: number }) => {
+  const { columns, setColumns } = useContext(BoardContext);
+
+  function deleteColumn() {
+    const tempColumns = columns;
+    tempColumns.splice(index, 1);
+    setColumns([...tempColumns]);
+  }
+
+  function enableDroppingCard(event: DragEvent<HTMLDivElement>) {
+    event.preventDefault();
+  }
+
+  function handleDropCard(event: DragEvent<HTMLDivElement>) {
+    // obtain dropped card info
+    const cardId = event.dataTransfer.getData("id");
+
+    // find dropped card in cards list
+    const { card1Loc: draggedCardLoc } = findCardsInColumns({
+      columns: columns,
+      card1Id: cardId,
+    });
+
+    console.log(draggedCardLoc);
+
+    // update dropped card list based on where it was dropped
+    let newColumns = columnsAfterMove({
+      columns: columns,
+      initialLoc: draggedCardLoc!,
+      finalLoc: { col: index },
+    });
+    setColumns([...newColumns]);
+  }
+
+  let cards: CardInfo[] = columns[index].cards;
+
+  return (
+    <div
+      css={css`
+        user-select: none;
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+
+        margin: 10px;
+        padding-bottom: 30px;
+        width: 200px;
+        min-height: 100px;
+
+        border-radius: var(--border-radius);
+        background-color: var(--highlight-soft);
+      `}
+      onDragOver={enableDroppingCard}
+      onDrop={handleDropCard}
+    >
+      {/* title */}
+      <h3
+        css={css`
+          width: 80%;
+          border-bottom: 2px solid;
+        `}
+      >
+        {columns[index].title}
+      </h3>
+
+      {/* cards */}
+      {cards.length > 0 ? (
+        cards.map((card, index) => (
+          <BoardCard key={`board-card-${index}-${card.id}`} info={card} />
+        ))
+      ) : (
+        <p>no cards</p>
+      )}
+
+      {/* settings */}
+      <div
+        css={css`
+          cursor: pointer;
+
+          position: absolute;
+          top: 6px;
+          right: 6px;
+
+          display: flex;
+          align-items: center;
+          justify-content: center;
+
+          width: 20px;
+          height: 30px;
+
+          border-radius: var(--border-radius);
+
+          transition: background-color 0.2s ease;
+
+          :hover {
+            background-color: var(--highlight-soft);
+          }
+        `}
+        onClick={deleteColumn}
+      >
+        X
+      </div>
+    </div>
+  );
+};
+
+export default BoardColumn;
---
+++ content/utilities/boardHelpers.ts
@@ -0,0 +1,68 @@
+import { Column } from "../components/board/BoardColumn";
+
+type Location = {
+  row?: number;
+  col?: number;
+};
+
+export function findCardsInColumns({
+  columns,
+  card1Id: cardId1,
+  card2Id: cardId2,
+}: {
+  columns: Column[];
+  card1Id: string;
+  card2Id?: string;
+}) {
+  let card1Loc: Location | undefined = undefined;
+
+  let card2Loc: Location | undefined = undefined;
+
+  for (let c = 0; c < columns.length; c++) {
+    for (let r = 0; r < columns[c].cards.length; r++) {
+      // find card 1
+      if (cardId1 === columns[c].cards[r].id) {
+        card1Loc = { col: c, row: r };
+      }
+
+      // find card 2
+      if (cardId2 === columns[c].cards[r].id) {
+        card2Loc = { col: c, row: r };
+      }
+
+      if (card1Loc && card2Loc) {
+        break;
+      }
+    }
+  }
+
+  return { card1Loc, card2Loc };
+}
+
+export function columnsAfterMove({
+  columns,
+  initialLoc,
+  finalLoc,
+}: {
+  columns: Column[];
+  initialLoc: Location;
+  finalLoc: Location;
+}) {
+  let tempCards = columns;
+
+  const removedCard = tempCards[initialLoc.col!].cards.splice(
+    initialLoc.row!,
+    1
+  )[0];
+
+  // move it to specified index
+  if (finalLoc.col) {
+    tempCards[finalLoc.col].cards.splice(finalLoc.row!, 0, removedCard);
+  }
+  // move card to end of list
+  else {
+    tempCards[finalLoc.col!].cards.push(removedCard);
+  }
+
+  return tempCards;
+}
