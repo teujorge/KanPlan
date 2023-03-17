6b0e06abe9b90cc321bb6fb4ea89679b7599ec3c
Matheus Jorge <mrljorge@outlook.com>
2023-03-16 21:54:04 -0300

    create header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,40 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { createContext, useState } from "react";
+import Layout from "../content/Layout";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+export enum Pages {
+  index = "index",
+  home = "home",
+  workspace = "workspace",
+  board = "board",
+  settings = "settings",
+}
+
+export const AppContext = createContext({
+  page: Pages.index,
+  setPage: (page: Pages) => {},
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [page, setPage] = useState(Pages.index);
+
+  function setPageWrapper(newPage: Pages) {
+    if (newPage !== page) {
+      setPage(newPage);
+    }
+  }
+
+  return (
+    <AppContext.Provider
+      value={{
+        page: page,
+        setPage: setPageWrapper,
+      }}
+    >
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AppContext.Provider>
+  );
 }
